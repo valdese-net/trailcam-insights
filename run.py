1a3eebb885242927b4cae1a7626bb8db01881e49
Noteworthy Software, Inc <online@noteworthysoftware.com>
2023-06-23 10:39:06 -0400

    Add debug prefix using a lambda function

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from lib.trailcam_findmax import *
 
 scanner = TrailcamFindMax()
 
-scanner.setDebug(print)
+scanner.setDebug(lambda msg: print('debug:',msg))
 
 fn = input("Video Path: ")
 stats = scanner.scan(fn)
